16c28944e0a9850ed443f90b5e182b794d86a4ed
mery0595 <bkmeryem110@gmail.com>
2023-06-16 01:03:14 +0100

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<studio.sh>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a peice of art, \n");
+	return (0);
+}
