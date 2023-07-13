258ce534aec58ad2754f601c8aa6724260945b8f
mery0595 <bkmeryem110@gmail.com>
2023-07-13 22:34:57 +0100

    More malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,46 @@
-#include "main.h"
-/**
- * string_nconcat -  function that concatenates two strings
- * @s1: The first string to concatenate
- * @s2: The second string to concatenate
- * @n: The of bytes to concatenate from s2
- * Return: NULL if concatenation fails
- * or a pointer to the newly-allocated space in memory
- */
+#include "holberton.h"
+#include <stdlib.h>
 
+/**
+ * string_nconcat - allocates a block of memory to store concatenated strings.
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the number of s2 to concatenate to s1
+ *
+ * Return: a pointer to allocated memory block.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
-	char *concat_str;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (n >= len2)
-		n = len2;
-
-	concat_str = malloc(sizeof(char) * (len1 + n + 1));
-
-	if (concat_str == NULL)
+	if (!s)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
-		concat_str[i] = s1[i];
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-	for (j = 0; j < n; j++)
-		concat_str[i + j] = s2[j];
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	concat_str[i + j] = '\0';
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	return (concat_str);
+	s[i] = '\0';
+
+	return (s);
 }
 
